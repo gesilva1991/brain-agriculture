cd5b29fc896f8087ba17ab6927e3f16318cd7d4e
george <ge_silva1991@hotmail.com>
2024-08-22 01:30:13 -0300

    ajust config services

---
+++ devtools/entrypoint-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "estou no entrypoint"
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+gunicorn app.wsgi:application --bind 0.0.0.0:8000 --log-level debug
+
